584c78ec9a0cd55faa54b592784ebb84df4c9aff
dev-ahmedali <ahmedali69325@gmail.com>
2022-11-08 23:34:57 +0600

    update immutability

--- react-redux/lession-2/immutabilty.js
+++ react-redux/lession-2/immutabilty.js
@@ -4,10 +4,18 @@ let color_lawn = {
   rating: 0,
 };
 
-function rateColor(color, rating) {
-  color.rating = rating;
-  return color;
-}
+// function rateColor(color, rating) {
+//   color.rating = rating;
+//   return color;
+// }
 
-console.log(rateColor(color_lawn, 5).rating);
-console.log(color_lawn.rating);
+// console.log(rateColor(color_lawn, 5).rating);
+// console.log(color_lawn.rating);
+
+
+
+var rateColor = function(color, rating) {
+  return Object.assign({}, color, {rating:rating})
+}
+console.log(rateColor(color_lawn, 5).rating)
+console.log(color_lawn.rating)
